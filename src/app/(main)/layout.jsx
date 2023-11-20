d7df6881940760ab9e2f69ec2435688c7ab42a96
Matthias Streicher <matthias-streicher@hotmail.com>
2023-11-20 22:30:46 +0100

    jljl

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -9,6 +9,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="de">
       <head>
+        <meta name="googlebot" content="notranslate" />
+      	<meta name="google-site-verification" content="1rlxQj4-jGrXnv2P7LL9tyeoeHWXHocT55W0-1Ci-ds"></meta>
         <link rel="icon" href="/images/icon.png" sizes="any" />
         <ColorSchemeScript />
       </head>
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,105 @@
 export default function sitemap() {
-    return [
-      {
-        url: '/',
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 1,
-      },
-      
-      {
-        url: 'https://acme.com/about',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
+  return [
+    {
+      url: "/",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
 
-      {
-        url: '/impressum',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.,
-      },
-    ]
+    {
+      url: "/taetigkeiten/allgemeines",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "/taetigkeiten/einsaetze",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "/taetigkeiten/uebungen",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "/taetigkeiten/jugend",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "/taetigkeiten/archiv",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+
+    {
+      url: "/buergerservice/sirenensignale",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.9,
+    },
+
+    {
+      url: "/buergerservice/notruf",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.9,
+    },
+    {
+      url: "/buergerservice/rettungsgasse",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.9,
+    },
+    {
+      url: "/buergerservice/richtig-loeschen",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.9,
+    },
+    {
+      url: "/buergerservice/abschnitt-schwechat-land",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.9,
+    },
+
+    {
+      url: "/feuerwehr/kommando",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "/feuerwehr/mannschaft",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "/feuerwehr/fuhrpark",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "/feuerwehr/ausruestung",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: "/impressum",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+  ];
 }
